87c338fa1c826c2bb25dd367e8f8fc3d1002fe4b
Jesús Sosa Jiménez <sosajesusjmz@gmail.com>
2025-11-05 21:38:36 -0300

    Update vite.config.js

--- azzar-frontend/vite.config.js
+++ azzar-frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/v1': {
+        target: 'http://192.168.100.91:8080', // tu backend HTTP
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
